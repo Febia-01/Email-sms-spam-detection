42ec126b9ef07be67b6af2690d2de08bcb54e1ef
Febia-01 <125859131+Febia-01@users.noreply.github.com>
2025-05-09 01:18:49 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+# Install NLTK
+pip install nltk
+
+# Create NLTK data directory
 mkdir -p ~/.nltk_data
+
+# Download required NLTK data
 python -m nltk.downloader -d ~/.nltk_data punkt stopwords
